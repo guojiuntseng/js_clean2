4e6facb2deefc215f4830c2c865d39822c701bfc
曾國峻 <guojiun@cengguojuns-MacBook-Air.local>
2018-10-19 10:46:02 +0800

    Reorder validators

--- src/domain/validator.js
+++ src/domain/validator.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 
 export const notEmpty = value => value !== ''
-export const emptyError = field => `${upperFirst(field)} cannot be empty`
 export const format = value => (/^\d{4}-\d{2}$/g).test(value)
-export const formatError = field => `Invalid ${field} format`
 export const positiveNumber = value => !isNaN(parseInt(value)) && value >= 0
+
+export const emptyError = field => `${upperFirst(field)} cannot be empty`
+export const formatError = field => `Invalid ${field} format`
 export const positiveError = field => `Invalid ${field}`
 
 export const NOT_EMPTY = {validate: notEmpty, error: emptyError};
